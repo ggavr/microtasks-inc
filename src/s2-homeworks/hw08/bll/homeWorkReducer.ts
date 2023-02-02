099668112b0ae65725c43f02c749cb379ac891dd
ggavr <gimer.gavr@gmail.com>
2023-02-02 13:11:02 +0100

    Commit hw-9

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -7,13 +7,18 @@ type ActionType =
 export const homeWorkReducer = (state: UserType[], action: ActionType): any => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
+            return action.payload === "up"
+                ?state.map((el, i)=> el).sort((a,b)=>a.name.localeCompare(b.name))
+                :state.map((el, i)=> el).sort((a,b)=>b.name.localeCompare(a.name))
 
-            return action.payload === "up" ? [...state.sort((a,b)=>a.name.localeCompare(b.name))]
-                : [...state.sort((a,b)=>b.name.localeCompare(a.name))]  // need to fix
+
+                // ? [...state.sort((a,b)=>a.name.localeCompare(b.name))]
+                // : [...state.sort((a,b)=>b.name.localeCompare(a.name))]  // need to fix
         }
         case 'check': {
 
-            return state.filter(el=>el.age>=18) // need to fix
+            return state.map(el=>el).filter(el=>el.age>=18)
+                // .sort((a,b)=>a.age-b.age) // need to fix
         }
         default:
             return state
