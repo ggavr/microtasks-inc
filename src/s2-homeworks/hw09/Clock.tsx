a42aee1f7156ca112c231f3c461c65458cb0256c
ggavr <gimer.gavr@gmail.com>
2023-02-04 11:29:38 +0100

    Commit hw-9 upd

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -14,11 +14,14 @@ function Clock() {
         // сохранить ид таймера (https://learn.javascript.ru/settimeout-setinterval#setinterval)
 
         setTimerId(+setInterval(()=>setDate(new Date(Date.now())), 1000))
+        console.log(timerId)
     }
 
     const stop = () => {
         // пишут студенты // поставить часы на паузу, обнулить ид таймера (timerId <- undefined)
-        clearInterval(timerId)
+
+       setTimerId(+clearInterval(timerId))
+        console.log(timerId)
     }
 
     const onMouseEnter = () => { // пишут студенты // показать дату если наведена мышка
@@ -67,14 +70,14 @@ setShow(false)
             <div className={s.buttonsContainer}>
                 <SuperButton
                     id={'hw9-button-start'}
-                    disabled={false} // пишут студенты // задизэйблить если таймер запущен
+                    disabled={!!timerId} // пишут студенты // задизэйблить если таймер запущен
                     onClick={start}
                 >
                     start
                 </SuperButton>
                 <SuperButton
                     id={'hw9-button-stop'}
-                    disabled={false} // пишут студенты // задизэйблить если таймер не запущен
+                    disabled={!timerId} // пишут студенты // задизэйблить если таймер не запущен
                     onClick={stop}
                 >
                     stop
