b83e3e443fe92851a6affd2bcd28442915732ccf
ggavr <gimer.gavr@gmail.com>
2023-02-27 12:04:51 +0100

    Commit hw-12 upd

--- src/s2-homeworks/hw12/HW12.tsx
+++ src/s2-homeworks/hw12/HW12.tsx
@@ -25,7 +25,7 @@ const HW12 = () => {
     const themeId = useSelector<AppStoreType, number>(state => state.theme.themeId)
 
     const change = (id: number) => { // дописать функцию
-    dispatch(changeThemeId(id))
+    dispatch(changeThemeId(themes[id-1].id))
     }
 
     useEffect(() => {
