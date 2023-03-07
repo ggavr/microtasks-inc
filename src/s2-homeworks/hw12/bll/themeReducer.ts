e4509072e2890ef69634edec4daf82ce8774b7c5
ggavr <gimer.gavr@gmail.com>
2023-03-07 18:43:38 +0100

    Commit hw-12 upd

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -10,7 +10,7 @@ export const themeReducer = (state = initState, action: ChangeThemeActionType): 
     switch (action.type) {
         // дописать
         case "SET_THEME_ID":
-        return {...state, themeId: action.id}
+        return {...state, themeId: Number(action.id)}
         default:
             return state
     }
